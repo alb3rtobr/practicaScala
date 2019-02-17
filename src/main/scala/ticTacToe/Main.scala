3f43b43385ea120f1ce2952bf49662906cf0db74
Alberto Bustamante <albertobr@gmail.com>
2019-02-18 00:31:34 +0100

    Actors - Forgot to add Main.scala

--- src/main/scala/ticTacToe/Main.scala
+++ src/main/scala/ticTacToe/Main.scala
@@ -1,23 +1,15 @@
 package ticTacToe
 
-import ticTacToe.models.Game
-import ticTacToe.views.{GameView, CoordinateView, GestorIO}
+import ticTacToe.models.{Game, Play, Player1, Player2}
+import ticTacToe.views.{CoordinateView, GameView, GestorIO}
+import akka.actor.{ActorSystem, Props}
 
-object Main {
+object Main extends App{
+    var game = new Game
+    val system = ActorSystem("system")
+    val player2 = system.actorOf(Props[Player2], name = "player2")
+    val player1 = system.actorOf(Props(new Player1(player2)), name = "player1")
+    player1 ! Play(game)
 
-  var game = new Game
 
-  def main(args: Array[String]): Unit = {
-
-    GameView.write(game)
-    do {
-      if (!game.isComplete){
-        game = game.put(CoordinateView.read)
-      } else {
-        game = game.move(CoordinateView.read, CoordinateView.read)
-      }
-      GameView.write(game)
-    } while (!game.isTicTacToe)
-    GestorIO.write("... pero has perdido")
-  }
 }
